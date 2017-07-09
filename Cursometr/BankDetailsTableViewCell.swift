0de14b2bf6657974172f0879c4efad6bf6aa9180
iMacAir <imacair@MacBook-Air-iMacAir.local>
2017-07-09 16:17:11 +0300

    Created the UITableViewController

---
+++ Cursometr/BankDetailsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BankDataTableViewCell.swift
+//  Cursometr
+//
+//  Created by iMacAir on 09.07.17.
+//  Copyright © 2017 iMacAir. All rights reserved.
+//
+
+import UIKit
+
+class BankDataTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
